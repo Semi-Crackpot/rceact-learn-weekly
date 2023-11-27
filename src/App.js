ce40f4a4d201fa6b239963bb6175a7f77a94984f
badouyao <badouyao@pinduoduo.com>
2023-11-27 21:37:29 +0800

    feat: router + md

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ const Main = () => {
   return (
     <div>
       目录
+      这是一句测试的话
       <ul>
       <li>
           <Link to="/2023/W1">2023#1</Link>
@@ -31,7 +32,6 @@ const App = () => {
         <Route path="2023/W1" element={<MdFile2023W1 />}></Route>
         <Route path="2023/W2" element={<MdFile2023W2 />}></Route>
         <Route path="2023/W3" element={<MdFile2023W3 />}></Route>
-        {/* <Route path="*" element={Main} /> */}
       </Routes>
     </BrowserRouter>
   );
