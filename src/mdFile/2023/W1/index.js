61195467159aab68721bbdc4a45930a99ad6c339
badouyao <badouyao@pinduoduo.com>
2023-12-07 20:28:27 +0800

    Updates

--- src/mdFile/2023/W1/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import MDIndex from './index.md';
-import ReactMarkdown from 'react-markdown';
-
-const MdFile2023W1 = () => {
-  return <ReactMarkdown children={MDIndex}/>
-}
-
-export default MdFile2023W1
