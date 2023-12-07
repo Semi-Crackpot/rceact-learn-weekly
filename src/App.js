f3de48fbcdbb6d40e12e762639dabdaa661e720d
badouyao <badouyao@pinduoduo.com>
2023-12-07 20:24:31 +0800

    feat: w4

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { createRoot } from "react-dom/client";
 import MdFile2023W1 from "./mdFile/2023/W1";
 import MdFile2023W2 from "./mdFile/2023/W2";
 import MdFile2023W3 from "./mdFile/2023/W3";
-import MdFile2023W4 from "./mdFile/2023/W3";
+import MdFile2023W4 from "./mdFile/2023/W4";
 
 const Main = () => {
   return (
     <div>
-      目录 这是一句测试的话
+      目录
       <ul>
         <li>
           <Link to="/2023/W1">2023#1</Link>
